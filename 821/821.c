267fe176097be6b7c7b700a0a427a1c38fc5e64b
userjack64 <3393450287@qq.com>
2023-08-21 13:54:58 +0800

    添加项目文件。

--- 821/821.c
+++ 821/821.c
@@ -9,8 +9,10 @@
 //void bubble_sort(int arr[],int len)
 //{
 //	int temp;
+//	
 //	for (int i = 1; i <= len - 1; i++)
 //	{
+//		int flag = 1;
 //		for (int j = 1; j < 9 - i; j++)
 //		{
 //			if (arr[j] > arr[j + 1])
@@ -18,15 +20,24 @@
 //				temp = arr[j];
 //				arr[j] = arr[j + 1];
 //				arr[j + 1] = temp;
+//				flag = 0;
 //			}
 //		}
+//		if (flag == 1)
+//		{
+//			break;
+//		}
 //	}
+//	
 //}
 
 int main()
 {
 
-	//bubble sorting
+
+
+
+	////bubble sorting
 	//int arr[] = { 1,3,5,7,9,2,4,6,8,10 };
 	//int len = sizeof(arr) / sizeof(arr[0]);
 	//bubble_sort(arr,len);
