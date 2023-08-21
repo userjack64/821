a8d5cf151ab17ced4c2e192699585b3b0a393917
userjack64 <3393450287@qq.com>
2023-08-21 13:54:58 +0800

    添加项目文件。

---
+++ 821/821.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+
+
+//bubble sorting
+//void bubble_sort(int arr[],int len)
+//{
+//	int temp;
+//	for (int i = 1; i <= len - 1; i++)
+//	{
+//		for (int j = 1; j < 9 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//}
+
+int main()
+{
+
+	//bubble sorting
+	//int arr[] = { 1,3,5,7,9,2,4,6,8,10 };
+	//int len = sizeof(arr) / sizeof(arr[0]);
+	//bubble_sort(arr,len);
+	//for (int i = 0; i < len; i++)
+	//{
+	//	printf(" arr[%d]=%d", i, arr[i]);
+	//}
+	//printf("\n");
+
+
+
+
+	//two-dimensional array
+	//int arr1[2][4] = {	{1,2,3,4}, 
+	//					{5,6,7,8}	};
+	//for (int i = 0; i < 2; i++)
+	//{
+	//	for (int j = 0; j < 4; j++)
+	//	{
+	//		printf(" arr[%d][%d]=%d", i, j,arr1[i][j]);
+	//	}
+	//	printf("\n");
+	//}
+
+	return 0;
+
+}
